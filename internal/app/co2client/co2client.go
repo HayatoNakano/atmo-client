6c50285afaa364532e712a0e73e8df39686395db
hn <35862888+HayatoNakano@users.noreply.github.com>
2024-06-11 22:20:00 +0900

    feat: create read func

--- internal/app/co2client/co2client.go
+++ internal/app/co2client/co2client.go
@@ -1,16 +1,64 @@
 package co2client
 
 import (
+	"bufio"
+	"fmt"
+	"log"
+	"regexp"
+	"strconv"
+	"time"
+
 	"github.com/tarm/serial"
 )
 
-const dev = "/dev/ttyACM0"
-
-func Open() (*serial.Port, error) {
-	c := &serial.Config{Name: dev, Baud: 115200}
-	return serial.OpenPort(c)
+type values struct {
+	co2 float64
+	hum float64
+	tmp float64
 }
 
-func read(output string) (co2 float64, hum float64, tmp float64, err error) {
-	return 1, 1, 1, nil
+type client struct {
+	dev string
+	re  *regexp.Regexp
+}
+
+func (c *client) open() (*serial.Port, error) {
+	return serial.OpenPort(&serial.Config{Name: c.dev, Baud: 115200})
+}
+
+func (c *client) Start() {
+	s, err := c.open()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	reader := bufio.NewReader(s)
+
+	for {
+		line, _, err := reader.ReadLine()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		c.read(string(line))
+		time.Sleep(1 * time.Second)
+	}
+
+}
+
+func (c *client) read(line string) (value *values, err error) {
+	matches := c.re.FindStringSubmatch(line)
+	if len(matches) != 4 {
+		return nil, fmt.Errorf("invalid format: %s", line)
+	}
+
+	var result [3]float64
+	for i, m := range matches[1:4] {
+		result[i], err = strconv.ParseFloat(m, 64)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &values{result[0], result[1], result[2]}, nil
 }
--- internal/app/co2client/co2client_test.go
+++ internal/app/co2client/co2client_test.go
@@ -1,30 +1,33 @@
 package co2client
 
 import (
+	"regexp"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestReading(t *testing.T) {
-	type values struct {
-		co2 float64
-		hum float64
-		tmp float64
+	type testcase struct {
+		expectedErr string
+		input       string
+		values      values
 	}
 
-	type testcase struct {
-		input  string
-		values values
-	}
+	c := client{re: regexp.MustCompile(`CO2=(?P<co2>\d+),HUM=(?P<hum>\d+\.\d+),TMP=(?P<tmp>-?\d+\.\d+)`)}
 
 	for _, e := range []testcase{
-		{"CO2=497,HUM=42.0,TMP=29.3", values{co2: 497, hum: 42.0, tmp: 29.3}},
-		{"CO2=731,HUM=44.4,TMP=29.7", values{co2: 731, hum: 44.4, tmp: 29.7}},
+		{"", "CO2=497,HUM=42.0,TMP=29.3", values{co2: 497, hum: 42.0, tmp: 29.3}},
+		{"", "CO2=731,HUM=44.4,TMP=29.7", values{co2: 731, hum: 44.4, tmp: 29.7}},
+		{"invalid format: ", "", values{}},
+		{"invalid format: STP", "STP", values{}},
 	} {
-		co2, hum, tmp, err := read(e.input)
-		a := values{co2, hum, tmp}
-		assert.Nil(t, err)
-		assert.EqualValues(t, e.values, a)
+		a, err := c.read(e.input)
+		if e.expectedErr != "" {
+			assert.EqualError(t, err, e.expectedErr)
+		} else {
+			assert.Nil(t, err)
+			assert.EqualValues(t, e.values, *a)
+		}
 	}
 }
